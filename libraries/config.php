fb9f2711a8eb5e05627922d6dc2a1bc4986a382f
Sok Kim Thanh <thanhsk1991@gmail.com>
2024-07-09 14:00:40 +0700

    Đọc file và ghi chú file

--- libraries/config.php
+++ libraries/config.php
@@ -1,123 +1,166 @@
+
 <?php
-	if(!defined('LIBRARIES')) die("Error");
-	
-	/* Root */
-	define('ROOT',__DIR__);
+/**
+ * 		Đoạn mã này chủ yếu thiết lập các thông số cấu hình cần thiết cho ứng dụng web,
+ * từ cấu hình cơ sở dữ liệu, thông tin công ty, thông tin đăng nhập, cấu hình 
+ * Google API, đến báo cáo lỗi và các tùy chỉnh khác.
+ * 		Các hằng số được định nghĩa và sử dụng để đảm bảo tính nhất quán và dễ dàng 
+ * thay đổi các giá trị cấu hình.
+ * 		Việc thiết lập múi giờ và kiểm tra định nghĩa của LIBRARIES giúp đảm bảo rằng
+ * ứng dụng hoạt động đúng theo yêu cầu.
+ * 		Các thông tin cấu hình như email, thông tin công ty, cơ sở dữ liệu, và các 
+ * cấu hình khác được tổ chức thành mảng $config để dễ dàng quản lý và truy cập.
+ */
+// Kiểm tra định nghĩa của LIBRARIES:
+// Nếu hằng số LIBRARIES chưa được định nghĩa, sẽ hiển thị thông báo lỗi và dừng thực thi mã.
+if (!defined('LIBRARIES')) die("Error");
 
-	/* Timezone */
-	date_default_timezone_set('Asia/Ho_Chi_Minh');
+//Định nghĩa ROOT: ROOT được định nghĩa là đường dẫn thư mục hiện tại của file.	
+/* Root */
+define('ROOT', __DIR__);
 
-	/* Cấu hình coder */
-	define('NN_MSHD','xxxx');
-	define('NN_AUTHOR','');
+// Thiết lập múi giờ: Thiết lập múi giờ mặc định là Asia/Ho_Chi_Minh.
+/* Timezone */
+date_default_timezone_set('Asia/Ho_Chi_Minh');
 
-	/* Cấu hình chung */
-	$config = array(
-		'customEmail' => 'co.khi.hoang.duong88@gmail.com', //email nhận mật khẩu
-		'copright' => array(// thông tin công ty tts
-			'name' => 'CÔNG TY TNHH CÔNG NGHỆ SOTA GROUP',
-			'email' => 'info@sotagroup.vn',
-			'dienthoai' => '0939.857.111',
-			'diachi' => 'Tòa Nhà HD Building 154 Phạm Văn Chiêu, Phường 9, Gò Vấp, Thành phố Hồ Chí Minh',
-			'website' => 'sotagroup.vn',
-			'worktime' => '8h - 17h từ thứ 2 đến thứ sáu, 8h - 12h sáng thứ bảy'
+// Định nghĩa các hằng số liên quan đến mã số hợp đồng (NN_MSHD) và tác giả (NN_AUTHOR).
+/* Cấu hình coder */
+define('NN_MSHD', 'xxxx');
+define('NN_AUTHOR', '');
+
+/* Cấu hình chung */
+// Đoạn code thiết lập một mảng cấu hình $config chứa nhiều thông tin cần thiết cho ứng dụng.
+$config = array(
+
+	//Thông tin email:
+	'customEmail' => 'co.khi.hoang.duong88@gmail.com', //email nhận mật khẩu
+
+	// Thông tin công ty:
+	'copright' => array( // thông tin công ty tts
+		'name' => 'CÔNG TY TNHH CÔNG NGHỆ SOTA GROUP',
+		'email' => 'info@sotagroup.vn',
+		'dienthoai' => '0939.857.111',
+		'diachi' => 'Tòa Nhà HD Building 154 Phạm Văn Chiêu, Phường 9, Gò Vấp, Thành phố Hồ Chí Minh',
+		'website' => 'sotagroup.vn',
+		'worktime' => '8h - 17h từ thứ 2 đến thứ sáu, 8h - 12h sáng thứ bảy'
+	),
+
+	// Thông tin tác giả:
+	'author' => array(
+		'name' => 'CÔNG TY TNHH CÔNG NGHỆ SOTA GROUP',
+		'email' => 'info@sotagroup.vn',
+		'timefinish' => 'Unknown'
+	),
+
+	// Thông tin SSL:
+	'arrayDomainSSL' => array(),
+
+	// Cấu hình cơ sở dữ liệu:
+	'database' => array(
+		'server-name' => $_SERVER["SERVER_NAME"],
+		'url' => '/',
+		'type' => 'mysql',
+		'host' => 'localhost',
+		'username' => 'root',
+		'password' => '',
+		'dbname' => '77-example',
+		'port' => 3306,
+		'prefix' => 'table_',
+		'charset' => 'utf8'
+	),
+
+	// Cấu hình website:
+	'website' => array(
+		'sendmail' => false,
+		'error-reporting' => false,
+		'secret' => '$tts@',
+		'salt' => 'swKJaajeS!t',
+		'debug-developer' => true,
+		'debug-css' => true,
+		'debug-js' => true,
+		'index' => false,
+		'upload' => array(
+			'max-width' => 1600,
+			'max-height' => 1600
 		),
-		'author' => array(
-			'name' => 'CÔNG TY TNHH CÔNG NGHỆ SOTA GROUP',
-			'email' => 'info@sotagroup.vn',
-			'timefinish' => 'Unknown'
+		'lang' => array(
+			'vi' => 'Tiếng Việt'
+			//'en'=>'Tiếng Anh'
 		),
-		'arrayDomainSSL' => array(),
-		'database' => array(
-			'server-name' => $_SERVER["SERVER_NAME"],
-			'url' => '/',
-			'type' => 'mysql',
-			'host' => 'localhost',
-			'username' => 'root',
-			'password' => '',
-			'dbname' => '77-example',
-			'port' => 3306,
-			'prefix' => 'table_',
-			'charset' => 'utf8'
+		'lang-doc' => 'vi|en',
+		'slug' => array(
+			'vi' => 'Tiếng Việt'
+			//'en'=>'Tiếng Anh'
 		),
-		'website' => array(
-			'sendmail' => false,
-			'error-reporting' => false,
-			'secret' => '$tts@',
-			'salt' => 'swKJaajeS!t',
-			'debug-developer' => true,
-			'debug-css' => true,
-			'debug-js' => true,
-			'index' => false,
-			'upload' => array(
-				'max-width' => 1600,
-				'max-height' => 1600
-			),
-			'lang' => array(
-				'vi'=>'Tiếng Việt'
-				//'en'=>'Tiếng Anh'
-			),
-			'lang-doc' => 'vi|en',
-			'slug' => array(
-				'vi'=>'Tiếng Việt'
-				//'en'=>'Tiếng Anh'
-			),
-			'seo' => array(
-				'vi'=>'Tiếng Việt'
-				//'en'=>'Tiếng Anh'
-			),
-			'comlang' => array(
-				"gioi-thieu" => array("vi"=>"gioi-thieu"),
-				"tin-tuc" => array("vi"=>"tin-tuc"),
-				"san-pham" => array("vi"=>"san-pham"),
-				"cong-trinh" => array("vi"=>"cong-trinh"),
-				"dich-vu" => array("vi"=>"dich-vu"),
-				"lien-he" => array("vi"=>"lien-he"),
-				"doi-tac" => array("vi"=>"doi-tac")
-			)
+		'seo' => array(
+			'vi' => 'Tiếng Việt'
+			//'en'=>'Tiếng Anh'
 		),
-		'order' => array(
-			'ship' => true,
-		),
-		'login' => array(
-			'admin' => 'LoginAdmin'.NN_MSHD,
-			'member' => 'LoginMember'.NN_MSHD,
-			'attempt' => 5,
-			'delay' => 15
-		),
-		'googleAPI' => array(
-			'recaptcha' => array(
-				'active' => true,
-				'urlapi' => 'https://www.google.com/recaptcha/api/siteverify',
-				'sitekey' => '6LfGof0hAAAAALA2D301jkz-MyM_PmVTdjMQSw2R',
-				'secretkey' => '6LfGof0hAAAAAKEnW5YqUc9V2ChTHsTGL6Hg-Trt'
-				//'sitekey' => '6Ld05qcZAAAAAJedNSmLEe1NOZdDtlYhwmltTIDC',
-				//'secretkey' => '6Ld05qcZAAAAAABH8BWbSiLnPoXTRXFReFDM7b8t'
-			)
-		),
-		'oneSignal' => array(
-			'active' => false,
-			'id' => 'af12ae0e-cfb7-41d0-91d8-8997fca889f8',
-			'restId' => 'MWFmZGVhMzYtY2U0Zi00MjA0LTg0ODEtZWFkZTZlNmM1MDg4'
-		),
-		'license' => array(
-			'version' => "7.0.0",
-			'powered' => "tts@congnghetts.vn"
+		'comlang' => array(
+			"gioi-thieu" => array("vi" => "gioi-thieu"),
+			"tin-tuc" => array("vi" => "tin-tuc"),
+			"san-pham" => array("vi" => "san-pham"),
+			"cong-trinh" => array("vi" => "cong-trinh"),
+			"dich-vu" => array("vi" => "dich-vu"),
+			"lien-he" => array("vi" => "lien-he"),
+			"doi-tac" => array("vi" => "doi-tac")
 		)
-	);
+	),
 
-	/* Error reporting */
-	error_reporting(($config['website']['error-reporting']) ? E_ALL : 0);
+	// Cấu hình đơn hàng:
+	'order' => array(
+		'ship' => true,
+	),
 
-	/* Cấu hình base */
-	$http = 'http://';
-	$config_url = $config['database']['server-name'].$config['database']['url'];
-	$config_base = $http.$config_url;
+	// Cấu hình đăng nhập:
+	'login' => array(
+		'admin' => 'LoginAdmin' . NN_MSHD,
+		'member' => 'LoginMember' . NN_MSHD,
+		'attempt' => 5,
+		'delay' => 15
+	),
 
-	/* Cấu hình login */
-	$login_admin = $config['login']['admin'];
-	$login_member = $config['login']['member']; 
+	// Cấu hình Google API:
+	'googleAPI' => array(
+		'recaptcha' => array(
+			'active' => true,
+			'urlapi' => 'https://www.google.com/recaptcha/api/siteverify',
+			'sitekey' => '6LfGof0hAAAAALA2D301jkz-MyM_PmVTdjMQSw2R',
+			'secretkey' => '6LfGof0hAAAAAKEnW5YqUc9V2ChTHsTGL6Hg-Trt'
+			//'sitekey' => '6Ld05qcZAAAAAJedNSmLEe1NOZdDtlYhwmltTIDC',
+			//'secretkey' => '6Ld05qcZAAAAAABH8BWbSiLnPoXTRXFReFDM7b8t'
+		)
+	),
 
-	/* Cấu hình upload */
-	require_once LIBRARIES."constant.php";
-?>
+	// Cấu hình OneSignal:
+	'oneSignal' => array(
+		'active' => false,
+		'id' => 'af12ae0e-cfb7-41d0-91d8-8997fca889f8',
+		'restId' => 'MWFmZGVhMzYtY2U0Zi00MjA0LTg0ODEtZWFkZTZlNmM1MDg4'
+	),
+
+	// Cấu hình giấy phép:
+	'license' => array(
+		'version' => "7.0.0",
+		'powered' => "tts@congnghetts.vn"
+	)
+);
+
+//Báo cáo lỗi:
+/* Error reporting */
+error_reporting(($config['website']['error-reporting']) ? E_ALL : 0);
+
+// Thiết lập các cấu hình cơ bản:
+/* Cấu hình base */
+$http = 'http://';
+$config_url = $config['database']['server-name'] . $config['database']['url'];
+$config_base = $http . $config_url;
+
+// Cấu hình đăng nhập:
+/* Cấu hình login */
+$login_admin = $config['login']['admin'];
+$login_member = $config['login']['member'];
+
+// Yêu cầu file constant.php:
+/* Cấu hình upload */
+require_once LIBRARIES . "constant.php";
--- libraries/constant.php
+++ libraries/constant.php
@@ -1,21 +1,45 @@
 <?php
+
+/**
+ * Đoạn mã này kiểm tra và tạo các thư mục cần thiết cho việc upload file trong ứng dụng web.
+ * Nó đảm bảo rằng các thư mục cần thiết luôn tồn tại và có quyền truy cập phù hợp (0777).
+ * File .htaccess được tạo trong thư mục gốc để chặn truy cập vào các file nhạy cảm, bảo vệ bảo mật cho ứng dụng.
+ * Các hằng số được định nghĩa động dựa trên tên thư mục, giúp việc truy cập các đường dẫn này trong mã nguồn dễ dàng hơn.
+ * Cách tổ chức mã và sử dụng các hàm kiểm tra, tạo thư mục và định nghĩa hằng số giúp mã rõ ràng, dễ bảo trì và mở rộng.
+ */
+// Kiểm tra định nghĩa của LIBRARIES: Nếu hằng số LIBRARIES chưa được định nghĩa,
+// sẽ hiển thị thông báo lỗi và dừng thực thi mã.
 if (!defined('LIBRARIES')) die("Error");
 
+// Thiết lập mảng thư mục và tên thư mục gốc cho upload:
+// $upload_const: tên thư mục gốc cho các thư mục upload.
+// $array_const: mảng chứa tên các thư mục con cần tạo bên trong thư mục gốc.
 /* Array folders */
 $upload_const = 'upload';
 $array_const = array('file', 'elfinder', 'sync', 'excel', 'seopage', 'photo', 'temp', 'user', 'product', 'color', 'news', 'tags', 'static');
 
+// Kiểm tra và tạo thư mục gốc nếu chưa tồn tại:
+// 	Kiểm tra xem thư mục gốc (upload_const) đã tồn tại chưa. 
+// Nếu chưa, tạo thư mục với quyền truy cập 0777.
 /* Define - Create folders upload */
 if (!file_exists(ROOT . '/../' . $upload_const)) {
 	mkdir(ROOT . '/../' . $upload_const, 0777, true);
 	chmod(ROOT . '/../' . $upload_const, 0777);
 }
 
+// Kiểm tra và tạo các thư mục con:
+
+// 	- Định nghĩa hằng số động cho các thư mục con:
+// Kiểm tra xem thư mục gốc đã tồn tại và mảng $array_const không rỗng.
+// Tạo file .htaccess bên trong thư mục gốc nếu chưa tồn tại:
+
 /* Define - Create folders childs */
 if (file_exists(ROOT . '/../' . $upload_const) && $array_const) {
 	$path_htaccess = ROOT . '/../' . $upload_const . '/.htaccess';
+	// Tạo file .htaccess bên trong thư mục gốc nếu chưa tồn tại:
 	if (!file_exists($path_htaccess)) {
 		$content_htaccess = '';
+		// Nội dung .htaccess: chặn truy cập vào các file nhạy cảm.
 		$content_htaccess .= '<Files ~ "\.(inc|sql|php|cgi|pl|php4|php5|asp|aspx|jsp|txt|kid|cbg|nok|shtml)$">' . PHP_EOL;
 		$content_htaccess .= 'order allow,deny' . PHP_EOL;
 		$content_htaccess .= 'deny from all' . PHP_EOL;
@@ -25,7 +49,7 @@ if (file_exists(ROOT . '/../' . $upload_const) && $array_const) {
 		fwrite($file_htaccess, $content_htaccess);
 		fclose($file_htaccess);
 	}
-
+	// Duyệt qua mảng $array_const để tạo các hằng số định nghĩa đường dẫn và tạo thư mục con nếu chưa tồn tại:
 	foreach ($array_const as $vconst) {
 		$define_upper_upload = strtoupper($upload_const);
 		$define_upper_const = strtoupper($vconst);
@@ -35,9 +59,12 @@ if (file_exists(ROOT . '/../' . $upload_const) && $array_const) {
 
 		// tạo hằng số định nghĩa UPLOAD_PHOTO_L nếu không có
 		if (!defined($define_upper_upload . '_' . $define_upper_const) && !defined($define_upper_upload . '_' . $define_upper_const . '_L')) {
+
+			// Định nghĩa các hằng số cho đường dẫn thư mục (UPLOAD_<TÊN THƯ MỤC>_L và UPLOAD_<TÊN THƯ MỤC>).
 			define($define_upper_upload . '_' . $define_upper_const, $define_in);
 			define($define_upper_upload . '_' . $define_upper_const . '_L', $define_out);
 
+			// Kiểm tra và tạo thư mục con với quyền truy cập 0777 nếu chưa tồn tại.
 			if (!file_exists(ROOT . '/../' . $upload_const . '/' . $define_lower_const)) {
 				mkdir(ROOT . '/../' . $upload_const . '/' . $define_lower_const, 0777, true);
 				chmod(ROOT . '/../' . $upload_const . '/' . $define_lower_const, 0777);
