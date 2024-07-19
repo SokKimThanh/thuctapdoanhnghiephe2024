b3ad1a107c5d9f8facca1ae21751f6453d5a0504
SokKimThanh <thanhsk1991@gmail.com>
2024-07-19 19:39:06 +0700

    báo cáo thực tập buổi 19

--- nitroland/public/script.js
+++ nitroland/public/script.js
@@ -24,3 +24,49 @@ btnSearch.addEventListener('click', () => {
 btnCloseSearch.addEventListener('click', () => {
     searchContainer.classList.remove('show-search-container');
 });
+
+/**
+ * Desktop Menu hover: nav-item hover active
+ */
+// Truy cập tất cả các phần tử nav-item
+// Ghi đè sự kiện click bằng sự kiện hover
+const navItems = document.querySelectorAll('.nav-item.dropdown');
+
+navItems.forEach(item => {
+    // Truy cập phần tử nav-link và home-link
+    const navLink = item.querySelector('.nav-link.dropdown-toggle');
+    const homeLink = item.querySelector('.dropdown-menu');
+
+    if (homeLink === null) return;
+
+    // Thêm sự kiện mouseover cho nav-link
+    navLink.addEventListener('mouseover', () => {
+        homeLink.classList.add('show');
+    });
+
+    // Thêm sự kiện mouseleave cho nav-item
+    item.addEventListener('mouseleave', () => {
+        homeLink.classList.remove('show');
+    });
+
+    // Thêm sự kiện mouseover cho home-link để giữ nó hiển thị
+    homeLink.addEventListener('mouseover', () => {
+        homeLink.classList.add('show');
+    });
+
+    // Thêm sự kiện mouseleave cho home-link để ẩn nó khi không hover
+    homeLink.addEventListener('mouseleave', () => {
+        homeLink.classList.remove('show');
+    });
+});
+/**
+ * Desktop Menu hover: homeLink hover show
+ */
+// Truy cập phần tử home-link đầu tiên
+const firstHomeLink = document.getElementsByClassName('home-link');
+
+// Thêm lớp CSS đặc biệt hoặc thay đổi style trực tiếp
+if (firstHomeLink) {
+    // Thêm lớp CSS đặc biệt 
+    firstHomeLink[0].classList.add('area-inside');
+}
